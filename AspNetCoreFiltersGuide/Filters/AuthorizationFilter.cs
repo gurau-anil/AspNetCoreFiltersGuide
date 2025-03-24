9137f537f5a3986880c1918345b26bb615149018
gurau-anil <gurau.anil@gmail.com>
2025-03-24 13:58:12 -0700

    Authorization Filter creation 1. Without parameter 2. With parameter 3. With dependencies.

---
+++ AspNetCoreFiltersGuide/Filters/AuthorizationFilter.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCoreFiltersGuide.Filters
+{
+    #region Authorization Filter with and without parameter
+    //Creating Authorization Filter without parameter
+    public class AuthorizationFilterWithoutParameter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            RequestHandler.HandleRequest(context);
+        }
+    }
+
+    //Creating Authorization Filter with parameter
+    public class AuthorizationFilterWithParameter : IAuthorizationFilter
+    {
+        private string _parameter1;
+        public AuthorizationFilterWithParameter(string parameter1)
+        {
+            _parameter1 = parameter1;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            RequestHandler.HandleRequest(context);
+        }
+    }
+
+    #endregion
+
+    #region Async Authorization Filter with and without parameter
+    //Creating Async Authorization Filter without parameter
+    public class AuthorizationAsyncFilterWithoutParameter : IAsyncAuthorizationFilter
+    {
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            RequestHandler.HandleRequest(context);
+        }
+    }
+
+    //Creating Async Authorization Filter with parameter
+    public class AuthorizationAsyncFilterWithParameter : IAsyncAuthorizationFilter
+    {
+        private string _parameter1;
+        public AuthorizationAsyncFilterWithParameter(string parameter1)
+        {
+            _parameter1 = parameter1;
+        }
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            RequestHandler.HandleRequest(context);
+        }
+    }
+
+    //Creating Async Authorization Filter with dependency and parameter
+    public class AuthorizationAsyncFilterWithParameterAndDependency : IAsyncAuthorizationFilter
+    {
+        private string _parameter1;
+        private readonly ILogger<AuthorizationAsyncFilterWithParameterAndDependency> _logger;
+        public AuthorizationAsyncFilterWithParameterAndDependency(ILogger<AuthorizationAsyncFilterWithParameterAndDependency> logger, string parameter1)
+        {
+            _logger = logger;
+            _parameter1 = parameter1;
+        }
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            _logger.LogInformation("Inside Authorization Filter with dependency and parameter");
+            RequestHandler.HandleRequest(context);
+        }
+    }
+    #endregion
+
+    public static class RequestHandler
+    {
+        public static void HandleRequest(AuthorizationFilterContext context)
+        {
+            if (new Random().Next(3) % 2 == 0)
+            {
+                context.Result = new JsonResult(new
+                {
+                    StatusCode = StatusCodes.Status403Forbidden,
+                    Message = "You do not have the required role to access this resource."
+                })
+                {
+                    StatusCode = StatusCodes.Status403Forbidden
+                };
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
