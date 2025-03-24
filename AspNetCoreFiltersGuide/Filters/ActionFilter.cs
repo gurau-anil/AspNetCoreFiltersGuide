a46169f2ef93cc1f799c73ed1eb3c8b5339cb16b
gurau-anil <gurau.anil@gmail.com>
2025-03-24 16:16:30 -0700

    Creating ActionFilter, which is used as a global filter

---
+++ AspNetCoreFiltersGuide/Filters/ActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCoreFiltersGuide.Filters
+{
+    public class ActionFilter
+    {
+    }
+
+public class LoggingActionFilter : IActionFilter
+    {
+        private readonly ILogger<LoggingActionFilter> _logger;
+
+        public LoggingActionFilter(ILogger<LoggingActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var actionName = context.ActionDescriptor.DisplayName;
+            _logger.LogInformation($"Action '{actionName}' is starting.");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var actionName = context.ActionDescriptor.DisplayName;
+            _logger.LogInformation($"Action '{actionName}' has completed.");
+        }
+    }
+
+}
--- AspNetCoreFiltersGuide/Program.cs
+++ AspNetCoreFiltersGuide/Program.cs
@@ -6,8 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Registering Filter, which is to be used as a service Filter. (Note: All the dependencies has to be registered as well)
 builder.Services.AddScoped<AuthorizationAsyncFilter>();
+builder.Services.AddControllers(options =>
+{
+    //Registering Global Filter, which will be applied to all the controllers
+    //Dependencies will be resolved automatically, however all the dependencies has to be registered
+    options.Filters.Add<LoggingActionFilter>();
 
-builder.Services.AddControllers();
+    // If parameters are passed manually then we have to create an instance
+
+    //using var serviceProvider = builder.Services.BuildServiceProvider();
+    //var logger = serviceProvider.GetRequiredService<ILogger<LoggingActionFilter>>();
+    //options.Filters.Add(new LoggingActionFilter(logger));
+
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
